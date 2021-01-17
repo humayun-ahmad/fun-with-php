1e36e023753058734a38f55221375a17458b26a6
humayun-ahmad <humayunraj789@gmail.com>
2021-01-17 19:47:25 +0600

    update file

--- data read and show/all_contact.php
+++ data read and show/all_contact.php
@@ -14,6 +14,11 @@ $data = mysqli_query($conn,$sql);
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+
+
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert.min.css">
+
+    
 </head>
 <body>
     <div class="container">
@@ -40,49 +45,42 @@ $data = mysqli_query($conn,$sql);
                 <td>
                     <a href="show.php?id=<?php echo $raw['id'] ?>" class="btn btn-info">Show</a>
                     <a href="edit.php?id=<?php echo $raw['id'] ?>" class="btn btn-primary">Edit</a>
-                    <a href="delete.php?id=<?php echo $raw['id'] ?>" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#exampleModal">Delete</a>
+                    <a href="delete.php?id=<?php echo $raw['id'] ?>" class="btn btn-danger" onclick="sweet()">Delete</a>
                 </td>
             </tr>
             <?php
                 }
             ?>
             </table>
-
+            
         </div>
     </div>
 
     <section class="pop-up-box">
-        <!-- Button trigger modal -->
-       
-
-        <!-- Modal -->
-        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog">
-            <div class="modal-content">
-            <div class="modal-header">
-                <h5 style="color:red" class="modal-title" id="exampleModalLabel" >Delete</h5>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <div class="modal-body">
-              Are you sure, You want to delete this data?
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
-                <?php
-                    while($raw = mysqli_fetch_assoc($data)){
-                ?>
-                <a href="delete.php?id=<?php echo $raw['id'] ?>"> Confirm</a>
-                <?php
-                    }
-                ?>
-
-            </div>
-            </div>
-        </div>
-        </div>
+    
     </section>
-
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js" integrity="sha384-q2kxQ16AaE6UbzuKqyBE9/u/KzioAlnx2maXQHiDX9d4/zp8Ok3f+M7DPm+Ib6IU" crossorigin="anonymous"></script>
+    
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js" integrity="sha384-q2kxQ16AaE6UbzuKqyBE9/u/KzioAlnx2maXQHiDX9d4/zp8Ok3f+M7DPm+Ib6IU" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-pQQkAEnwaBkjpqZ8RU1fF1AKtTcHJwFl3pblpTlHXybJjHpMYo79HY3hIi4NKxyj" crossorigin="anonymous"></script>
+
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert.min.js"></script>
+<script type="text/javascript">
+    function sweet() {
+        swal({
+            title: "Are you sure?",
+            text: "Your will not be able to recover this imaginary file!",
+            type: "warning",
+            showCancelButton: true,
+            confirmButtonClass: "btn-danger",
+            confirmButtonText: "Yes, delete it!",
+            closeOnConfirm: false
+        },
+        function(){
+        swal("Deleted!", "Your imaginary file has been deleted.", "success");
+        });
+    }
+</script>
+
 </body>
 </html>
